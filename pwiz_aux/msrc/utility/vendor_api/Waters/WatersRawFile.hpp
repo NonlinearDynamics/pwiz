34f80347a44d9e6e3a8305834f5ab2295164140a
Ian Morns <ian_morns@waters.com>
2023-07-25 11:10:55 +0100

    use ScanProcessor to perform centroding after DDA processing

--- pwiz_aux/msrc/utility/vendor_api/Waters/WatersRawFile.hpp
+++ pwiz_aux/msrc/utility/vendor_api/Waters/WatersRawFile.hpp
@@ -431,10 +431,9 @@ struct PWIZ_API_DECL RawData
 
     void EnableProcessing(bool bEnableDDAProcessing)
     {
+        ScanProcessor.SetRawData(Reader);
         if (bEnableDDAProcessing)
             DDAProcessor.SetRawData(Reader);
-        else
-            ScanProcessor.SetRawData(Reader);
     }   
 
     unsigned int GetDDAScanCount()
@@ -442,25 +441,31 @@ struct PWIZ_API_DECL RawData
         return DDAProcessor.GetScanCount();
     }
 
-    bool GetDDAScan(const int& nWhichIndex, float& RT, int& function, int& startScan, int& endScan, bool& isMS1, float& setMass, float& precursorMass, vector<float>& masses, vector<float>& intensities)
+    bool GetDDAScan(const int& nWhichIndex, float& RT, int& function, int& startScan, int& endScan, bool& isMS1, float& setMass, float& precursorMass, bool doCentroid, vector<float>& masses, vector<float>& intensities)
     {
         MassLynxParameters parameters;
         bool success = DDAProcessor.GetScan(nWhichIndex, masses, intensities, parameters);
 
-        if (success)
-        {
-            RT = lexical_cast<float>(parameters.Get(MassLynxDDAIndexDetail::RT));
-            function = lexical_cast<int>(parameters.Get(MassLynxDDAIndexDetail::FUNCTION));
-            startScan = lexical_cast<int>(parameters.Get(MassLynxDDAIndexDetail::START_SCAN));
-            endScan = lexical_cast<int>(parameters.Get(MassLynxDDAIndexDetail::END_SCAN));
-            isMS1 = lexical_cast<int>(parameters.Get(MassLynxDDAIndexDetail::SCAN_TYPE)) == (int)MassLynxScanType::MS1;
+        if (!success)
+            return success;
 
-            if (!isMS1)
-            {
-                setMass = lexical_cast<float>(parameters.Get(MassLynxDDAIndexDetail::SET_MASS));
-                precursorMass = lexical_cast<float>(parameters.Get(MassLynxDDAIndexDetail::PRECURSOR_MASS));
-            }
+        if (doCentroid)
+        {
+            ScanProcessor.SetScan(masses, intensities).Centroid().GetScan(masses, intensities);
         }
+        
+        RT = lexical_cast<float>(parameters.Get(MassLynxDDAIndexDetail::RT));
+        function = lexical_cast<int>(parameters.Get(MassLynxDDAIndexDetail::FUNCTION));
+        startScan = lexical_cast<int>(parameters.Get(MassLynxDDAIndexDetail::START_SCAN));
+        endScan = lexical_cast<int>(parameters.Get(MassLynxDDAIndexDetail::END_SCAN));
+        isMS1 = lexical_cast<int>(parameters.Get(MassLynxDDAIndexDetail::SCAN_TYPE)) == (int)MassLynxScanType::MS1;
+
+        if (!isMS1)
+        {
+            setMass = lexical_cast<float>(parameters.Get(MassLynxDDAIndexDetail::SET_MASS));
+            precursorMass = lexical_cast<float>(parameters.Get(MassLynxDDAIndexDetail::PRECURSOR_MASS));
+        }
+        
         return success;
     }
 
