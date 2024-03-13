5455988b9fe20c5e8dd63963a1f0990a6aee2a2e
Pete Reay <pete_reay@waters.com>
2024-03-13 16:18:06 +0000

    Write correct units into mz array when writing Waters spectra
    The spectrum list was already determining that the unit should be 'nanometre' in some cases, but this wasn't reflected in the array in the mzML

--- pwiz/data/msdata/MSData.cpp
+++ pwiz/data/msdata/MSData.cpp
@@ -820,7 +820,7 @@ PWIZ_API_DECL void Spectrum::setMZIntensityPairs(const MZIntensityPair* input, s
 
 /// set m/z and intensity arrays separately (they must be the same size) by swapping the vector contents
 /// this allows for a more nearly zero copy setup.  Contents of mzArray and intensityArray are undefined after calling.
-PWIZ_API_DECL void Spectrum::swapMZIntensityArrays(pwiz::util::BinaryData<double>& mzArray, pwiz::util::BinaryData<double>& intensityArray, CVID intensityUnits)
+PWIZ_API_DECL void Spectrum::swapMZIntensityArrays(pwiz::util::BinaryData<double>& mzArray, pwiz::util::BinaryData<double>& intensityArray, CVID intensityUnits, CVID mzUnits)
 {
     if (mzArray.size() != intensityArray.size())
         throw runtime_error("[MSData::Spectrum::swapMZIntensityArrays()] Sizes do not match.");
@@ -835,7 +835,7 @@ PWIZ_API_DECL void Spectrum::swapMZIntensityArrays(pwiz::util::BinaryData<double
     {
         bd_mz = BinaryDataArrayPtr(new BinaryDataArray);
         CVParam arrayType(MS_m_z_array);
-        arrayType.units = MS_m_z;
+        arrayType.units = mzUnits;
         bd_mz->cvParams.push_back(arrayType);
         binaryDataArrayPtrs.push_back(bd_mz);
     }
